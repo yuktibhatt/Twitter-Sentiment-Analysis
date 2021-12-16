a2cab549e303072f37efc40d0abb3e1a5aa8f64b
yuktibhatt <bhattyukti5@gmail.com>
2021-12-16 15:35:10 +0545

    db connections

--- api/app.py
+++ api/app.py
@@ -1,13 +1,19 @@
 from flask import Flask
+#from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password:1234@localhost/twitterSentiment'
+#db = SQLAlchemy(app)
 
 
 @app.route('/', methods=['GET'])
 def index():
-    pass
+    return {
+        'name' : 'Hello World'
+    }
 
-if __name__ == '__main__':
+if __name__ == 'main':
     app.run(debug=True)
 
 
